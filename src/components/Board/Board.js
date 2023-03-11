fd1f31a119debd047071d69c1c00af2b308d71b5
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-03-11 12:12:03 +0200

    Add Board header

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -87,7 +87,19 @@ const Board = () => {
   }
 
   return (
-    <article className='board'>{board}</article>
+    <article className='board-container'>
+      <div className="board-header">
+        <h3>
+          Player 1:
+          <span className="green"> X</span> 
+          &nbsp; &nbsp; &nbsp; 
+          Player 2:
+          <span className="orange"> O</span>
+        </h3>
+        <p>Get 3 marks in a row (up, down, across, or diagonally) to win the game.</p>
+      </div>
+      <div className='board'>{board}</div>
+    </article>
   );
 }
 
