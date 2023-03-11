f14e85b3223e69b7677ff58c8a61b2e1c8126197
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-03-11 11:16:02 +0200

    Create and render Display component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Board from './components/Board/Board';
+import Display from './components/Display/Display'
 
 function App() {
   return (
     <section className="App">
       <header className="App-header">
         <h1>React TicTacToe</h1>
-
-        <Board />
       </header>
+      <section className='game-container'>
+        <Board />
+        <Display />
+      </section>
     </section>
   );
 }
---
+++ src/components/Display/Display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Display() {
+  return (
+    <article className='display'>Display</article>
+  )
+}
+
+export default Display
