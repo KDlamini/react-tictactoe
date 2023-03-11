cd68aa1818b690e4609febaf6b47743ea713a2db
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-03-11 12:39:20 +0200

    Remove Board class component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,54 +4,6 @@ import Header from './Header'
 import calculateWinner from '../utils/calculateWinner'
 
 // A `Board` component that renders a 3x3 grid of `Square` components.
-export class Board1 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  // Keep track of the game state (i.e., which squares have been clicked and by whom) in its state.
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  // When a `Square` is clicked, update the game state in the `Board` component and re-render the affected `Square`.
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const boardSize = 3;
-    let squares = [];
-
-    for (let i = 0; i < boardSize; i++) {
-      let row = [];
-      for (let j = 0; j < boardSize; j++) {
-        row.push(this.renderSquare(i * boardSize + j));
-      }
-      squares.push(<div key={i} className="board-row">{row}</div>);
-    }
-
-    return (
-      <div>{squares}</div>
-    );
-  }
-}
-
-// A `Board` component that renders a 3x3 grid of `Square` components.
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
