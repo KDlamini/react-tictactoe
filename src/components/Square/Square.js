5395cc6c9d42c54f2ee36022dd8873ebece9dc08
Simo Nkosi <wilzmar.kwd@gmail.com>
2023-03-11 11:35:45 +0200

    Style Square tokens

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -3,13 +3,15 @@ import React from 'react'
 // A `Square` component that renders either an "X", "O", or nothing depending on its state.
 class Square extends React.Component {
   render() {
+    const token = this.props.value;
+
     return (
       <button
-        className="square"
+        className={`square ${token === "X" ? "green": "orange"}`}
         onClick={() => this.props.onClick()}
         disabled={this.props.isGameOver ? true : false}
       >
-        {this.props.value}
+        {token}
       </button>
     );
   }
